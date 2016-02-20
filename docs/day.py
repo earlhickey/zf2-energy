6f942e6c70098d7c4dc15e6faafd273c7c541cf4
Patrick Groot <pgroot@gmail.com>
2016-02-20 19:04:10 +0100

    Daily energy usage script accept date

--- docs/day.py
+++ docs/day.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python
 # SMARTMETER agregate data
-# calculate and safe yesterdays energy usage
+# calculate and safe daily energy usage
 # @author pG
-version = "1.1"
+version = "1.2"
 
 # needed libraries
-from datetime import date, timedelta
-import sqlite3
+import datetime
+import argparse
+import sqlite3, sys
 
-# get yesterdays date
-ts = date.today() - timedelta(1)
-yesterday = ts.strftime('%Y-%m-%d')
+# parse arguments or defaults
+parser = argparse.ArgumentParser()
+parser.add_argument("date", nargs='?', default=datetime.date.today().strftime("%Y-%m-%d"))
+args = parser.parse_args()
+
+# create date obj from str for conversions
+curdate = datetime.datetime.strptime(args.date, "%Y-%m-%d")
+
+# create dates and convert to string
+p = curdate - datetime.timedelta(1)
+prevdate = p.strftime('%Y-%m-%d')
+n = curdate + datetime.timedelta(1)
+nextdate = n.strftime('%Y-%m-%d')
+print curdate
+print prevdate
+print nextdate
+
 
 # get first record of today and yesterday
 conn = sqlite3.connect('/var/db/pg')
 c = conn.cursor()
 c.execute('''SELECT power_usage_low, power_usage_hi, power_return_low,power_return_hi, gas_usage, MIN(datetime)
   FROM energy
-  WHERE datetime > ?
+  WHERE datetime BETWEEN ? AND ?
   GROUP BY strftime('%Y-%m-%d', datetime)
-  ORDER BY datetime DESC;''', (yesterday,))
+  ORDER BY datetime DESC;''', (prevdate, nextdate))
 results = c.fetchall()
 
 # results should return 2 records
@@ -33,9 +48,10 @@ power_return_low = round(results[0][2] - results[1][2], 3)
 power_return_hi = round(results[0][3] - results[1][3], 3)
 gas_usage = round(results[0][4] - results[1][4], 3)
 
+print power_usage_low, power_usage_hi, power_return_low, power_return_hi, gas_usage, prevdate
 # insert agregated data
 q = '''INSERT INTO "energy_day" ("power_usage_low","power_usage_hi","power_return_low","power_return_hi","gas_usage","date") VALUES (?,?,?,?,?,?)'''
-c.execute(q, (power_usage_low, power_usage_hi, power_return_low, power_return_hi, gas_usage, yesterday))
+c.execute(q, (power_usage_low, power_usage_hi, power_return_low, power_return_hi, gas_usage, prevdate))
 conn.commit()
 c.close()
 conn.close()
