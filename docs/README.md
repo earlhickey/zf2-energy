f4908adac8e6d7d0307decab4e9b9a58f2343849
Patrick Groot <pgroot@gmail.com>
2016-03-02 11:27:53 +0100

    Database update

---
+++ docs/README.md
@@ -0,0 +1,20 @@
+queries
+
+SELECT
+    strftime('%Y-%m', date) as month,
+    ROUND(SUM("power_usage_low"), 2) as powerUsageLow,
+    ROUND(SUM("power_usage_hi"), 2) as powerUsageHi,
+    powerUsageLow+powerUsageHi as powerUsage,
+    ROUND(SUM("power_return_low"), 2) as power_return_low,
+    ROUND(SUM("power_return_hi"), 2) as power_return_hi,
+    ROUND(SUM("gas_usage"), 2) as gas_usage
+FROM "energy_day" GROUP BY month;
+
+CREATE VIEW "TotalsByHour" AS SELECT
+    strftime('%Y-%m-%d %H', datetime) as hour,
+    ROUND(SUM("power_usage_hi") + SUM("power_usage_low"), 2) as powerUsage,
+    ROUND(SUM("power_return_hi") + SUM("power_return_low"), 2) as powerReturn,
+    ROUND(SUM("gas_usage"), 2) as gasUsage
+FROM "energy"
+GROUP BY hour
+ORDER BY hour DESC;
--- docs/database.sql
+++ docs/database.sql
@@ -38,23 +38,14 @@ CREATE VIEW "TotalsByMonth" AS SELECT
     ROUND(SUM("gas_usage"), 2) as gasUsage
 FROM "energy_day" GROUP BY month;
 
----
-
-SELECT
-    strftime('%Y-%m', date) as month,
+CREATE VIEW "TotalsByYear" AS SELECT
+    strftime('%Y', date) as year,
     ROUND(SUM("power_usage_low"), 2) as powerUsageLow,
     ROUND(SUM("power_usage_hi"), 2) as powerUsageHi,
-    powerUsageLow+powerUsageHi as powerUsage,
-    ROUND(SUM("power_return_low"), 2) as power_return_low,
-    ROUND(SUM("power_return_hi"), 2) as power_return_hi,
-    ROUND(SUM("gas_usage"), 2) as gas_usage
-FROM "energy_day" GROUP BY month;
-
-CREATE VIEW "TotalsByHour" AS SELECT
-    strftime('%Y-%m-%d %H', datetime) as hour,
     ROUND(SUM("power_usage_hi") + SUM("power_usage_low"), 2) as powerUsage,
+    ROUND(SUM("power_return_low"), 2) as powerReturnLow,
+    ROUND(SUM("power_return_hi"), 2) as powerReturnHi,
     ROUND(SUM("power_return_hi") + SUM("power_return_low"), 2) as powerReturn,
     ROUND(SUM("gas_usage"), 2) as gasUsage
-FROM "energy"
-GROUP BY hour
-ORDER BY hour DESC;
+FROM "energy_day" GROUP BY year;
+
